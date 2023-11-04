54bb7a5a63fb4f4ed434dee7959f5a304b8b1016
jackson0731 <annf731@gmail.com>
2023-11-04 13:52:47 +0800

    暫停介面(Bug: 還是會繼續開槍)

---
+++ Star/Assets/Script/Pause.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseWindow;
+    public GameObject settingWindow;
+
+    [Header("Be Choose")]
+    [SerializeField] private GameObject resumeBeChoose;
+    [SerializeField] private GameObject settingBeChoose;
+    [SerializeField] private GameObject quitBeChoose;
+    [SerializeField] private GameObject returnBeChoose;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseWindow.activeSelf && !settingWindow.activeSelf)
+        {
+            pauseWindow.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void OnResumeClick()
+    {
+        pauseWindow.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void OnSettingClick()
+    {
+        pauseWindow.SetActive(false);
+        settingBeChoose.SetActive(false);
+        settingWindow.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void OnQuitClick()
+    {
+        SceneManager.LoadScene("Base");
+    }
+    public void OnReturnClick()
+    {
+        settingWindow.SetActive(false);
+        returnBeChoose.SetActive(false);
+        pauseWindow.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void OnResumePointEnter()
+    {
+        resumeBeChoose.SetActive(true);
+    }
+    public void OnSettingPointEnter()
+    {
+        settingBeChoose.SetActive(true);
+    }
+    public void OnQuitPointEnter()
+    {
+        quitBeChoose.SetActive(true);
+    }
+    public void OnReturnPointEnter()
+    {
+        returnBeChoose.SetActive(true);
+    }
+    public void OnResumePointExit()
+    {
+        resumeBeChoose.SetActive(false);
+    }
+    public void OnSettingPointExit()
+    {
+        settingBeChoose.SetActive(false);
+    }
+    public void OnQuitPointExit()
+    {
+        quitBeChoose.SetActive(false);
+    }
+    public void OnReturnPointExit()
+    {
+        returnBeChoose.SetActive(false);
+    }
+}
