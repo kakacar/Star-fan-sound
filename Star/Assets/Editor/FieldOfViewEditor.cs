ee671539885ba417c7e841627d8696d3179fd1b9
jackson0731 <annf731@gmail.com>
2023-11-04 14:56:00 +0800

    暫停介面Bug修復

--- Star/Assets/Editor/FieldOfViewEditor.cs
+++ Star/Assets/Editor/FieldOfViewEditor.cs
@@ -7,13 +7,16 @@ public class FieldOfViewEditor : Editor
     private void OnSceneGUI()
     {
         FieldOfView fov = (FieldOfView)target;
+        //眼睛位置
         Vector3 fovP = fov.transform.GetChild(2).gameObject.transform.position;
+        //視野範圍
         Handles.color = Color.white;
         Handles.DrawWireArc(fovP, Vector3.up, Vector3.forward, 360, fov.radius);
 
         Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
         Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.angle / 2);
 
+        //視野寬度
         Handles.color = Color.yellow;
         Handles.DrawLine(fovP, fovP + viewAngle01 * fov.radius);
         Handles.DrawLine(fovP, fovP + viewAngle02 * fov.radius);
