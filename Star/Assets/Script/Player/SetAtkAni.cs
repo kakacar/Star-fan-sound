8d35bf3fef39d68b88c4534e18b86ce67b52e867
Ben60602 <xudjox@gmail.com>
2023-12-08 13:58:08 +0800

    MC update

--- Star/Assets/Script/Player/SetAtkAni.cs
+++ Star/Assets/Script/Player/SetAtkAni.cs
@@ -63,7 +63,7 @@ namespace MonoBehaviours
             Destroy(CurrentweaponL);
 
             CurrentweaponR = Instantiate(Weapon[1], weaponHolderR.transform);
-            CurrentweaponR.transform.localPosition = new Vector3(0.05f, -0.2f, 0.015f);
+            CurrentweaponR.transform.localPosition = new Vector3(-0.001f, -0.265f, -0.002f);
             CurrentweaponR.transform.localRotation = Quaternion.Euler(-3f, -167f, -82f);
         }
 
@@ -76,11 +76,11 @@ namespace MonoBehaviours
             Destroy(CurrentweaponL);
 
             CurrentweaponR = Instantiate(Weapon[0], weaponHolderR.transform);
-            CurrentweaponR.transform.localPosition = new Vector3(0f, 0.076f, 0.012f);
+            CurrentweaponR.transform.localPosition = new Vector3(0.002f, 0.009f, 0.015f);
             CurrentweaponR.transform.localRotation = Quaternion.Euler(-180f, -1f, 1.4f);
 
             CurrentweaponL = Instantiate(Weapon[0], weaponHolderL.transform);
-            CurrentweaponL.transform.localPosition = new Vector3(0.004f, 0.072f, 0.023f);
+            CurrentweaponL.transform.localPosition = new Vector3(0.008f, 0.041f, 0.018f);
             CurrentweaponL.transform.localRotation = Quaternion.Euler(6f, 2.1f, -177f);
         }
 
