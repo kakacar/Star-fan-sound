576d3d5301be28f7b8039e99c8380604f1d11cb4
jackson0731 <annf731@gmail.com>
2023-12-20 17:41:14 +0800

    製作功能

--- Star/Assets/Script/Base/Craft.cs
+++ Star/Assets/Script/Base/Craft.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class Craft : MonoBehaviour
 {
     private Player player;
     public Text[] stuffCount;
+    public Button[] craftButton;
     private void Update()
     {
         stuffCount[0].text = "電路板" + "<br>" + "：" + player.stuff[0];
@@ -17,6 +20,74 @@ public class Craft : MonoBehaviour
         stuffCount[5].text = "生物DNA" + "<br>" + "：" + player.stuff[5];
         stuffCount[6].text = "複合金屬" + "<br>" + "：" + player.stuff[6];
         stuffCount[7].text = "能量精華" + "<br>" + "：" + player.stuff[7];
+        CanCraftOrNot();
+    }
+    private void CanCraftOrNot()
+    {
+        if (player.stuff[0]! <= 0 || player.stuff[1]! <= 0 || player.stuff[4]! <= 0)
+        {
+            craftButton[0].interactable = true;
+        }
+        else
+        {
+            craftButton[0].interactable = false;
+        }
+
+        if (player.stuff[5]! <= 0 || player.stuff[6]! <= 0 || player.stuff[7]! <= 0)
+        {
+            craftButton[1].interactable = true;
+        }
+        else
+        {
+            craftButton[1].interactable = false;
+        }
+
+        if (player.stuff[0]! <= 0 || player.stuff[1]! <= 0 || player.stuff[2]! <= 0)
+        {
+            craftButton[2].interactable = true;
+        }
+        else
+        {
+            craftButton[2].interactable = false;
+        }
+
+        if (player.stuff[3]! <= 0 || player.stuff[4]! <= 0 || player.stuff[5]! <= 0 || player.stuff[7]! <= 0)
+        {
+            craftButton[3].interactable = true;
+        }
+        else
+        {
+            craftButton[3].interactable = false;
+        }
+    }
+    public void Craft1()
+    {
+        player.stuff[0] -= 1;
+        player.stuff[1] -= 1;
+        player.stuff[4] -= 1;
+        player.item[0] += 1;
+    }
+    public void Craft2()
+    {
+        player.stuff[5] -= 1;
+        player.stuff[6] -= 1;
+        player.stuff[7] -= 1;
+        player.item[1] += 1;
+    }
+    public void Craft3()
+    {
+        player.stuff[0] -= 1;
+        player.stuff[1] -= 1;
+        player.stuff[2] -= 1;
+        player.item[2] += 1;
+    }
+    public void Craft4()
+    {
+        player.stuff[3] -= 1;
+        player.stuff[4] -= 1;
+        player.stuff[5] -= 1;
+        player.stuff[7] -= 1;
+        player.item[3] += 1;
     }
     public void CloseWindow()
     {
--- Star/Assets/Script/Base/Sell.cs
+++ Star/Assets/Script/Base/Sell.cs
@@ -24,28 +24,28 @@ public class Sell : MonoBehaviour
     private void Update()
     {
         moneyText.text = "" + player.money;
-        if (player.item[0] == 0)
+        if (player.item[0] <= 0)
         {
             sellButton[0].interactable = false;
         }else
         {
             sellButton[0].interactable = true;
         }
-        if (player.item[1] == 0)
+        if (player.item[1] <= 0)
         {
             sellButton[1].interactable = false;
         }else
         {
             sellButton[1].interactable = true;
         }
-        if (player.item[2] == 0)
+        if (player.item[2] <= 0)
         {
             sellButton[2].interactable = false;
         }else
         {
             sellButton[2].interactable = true;
         }
-        if (player.item[3] == 0)
+        if (player.item[3] <= 0)
         {
             sellButton[3].interactable = false;
         }else
@@ -55,24 +55,28 @@ public class Sell : MonoBehaviour
     }
     public void Sell1()
     {
+        player.item[0] -= 1;
         player.money += 400;
         sell[0].SetActive(false);
         noDeal[0].SetActive(true);
     }
     public void Sell2()
     {
+        player.item[1] -= 1;
         player.money += 350;
         sell[1].SetActive(false);
         noDeal[1].SetActive(true);
     }
     public void Sell3()
     {
+        player.item[2] -= 1;
         player.money += 400;
         sell[2].SetActive(false);
         noDeal[2].SetActive(true);
     }
     public void Sell4()
     {
+        player.item[3] -= 1;
         player.money += 600;
         sell[3].SetActive(false);
         noDeal[3].SetActive(true);
