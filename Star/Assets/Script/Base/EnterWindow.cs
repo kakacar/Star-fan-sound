6feccba6f5803ae7d9cfd4cc13c4145eaf7fc127
jackson0731 <annf731@gmail.com>
2023-12-22 02:51:47 +0800

    Bug fixed

--- Star/Assets/Script/Base/EnterWindow.cs
+++ Star/Assets/Script/Base/EnterWindow.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnterWindow : MonoBehaviour
 {
