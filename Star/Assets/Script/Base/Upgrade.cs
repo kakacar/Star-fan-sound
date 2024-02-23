9640b96c4a4d854ba785e35d9ae1a29e9b943958
jackson0731 <annf731@gmail.com>
2024-02-23 11:57:03 +0800

    升級功能完成

--- Star/Assets/Script/Base/Upgrade.cs
+++ Star/Assets/Script/Base/Upgrade.cs
@@ -6,19 +6,40 @@ using UnityEngine.EventSystems;
 
 public class Upgrade : MonoBehaviour
 {
-    private Player player;
     public Canvas canvas;
+    public Button[] upGrade;
+    public Text info;
     public void HpUpgrade()
     {
-
+        upGrade[0].interactable = false;
+        upGrade[1].interactable = true;
+        upGrade[2].interactable = true;
+        upGrade[3].interactable = true;
+        info.text = "1";
     }
     public void CollectUpgrade()
     {
-        Debug.Log("Collect Upgrade");
+        upGrade[0].interactable = true;
+        upGrade[1].interactable = false;
+        upGrade[2].interactable = true;
+        upGrade[3].interactable = true;
+        info.text = "2";
     }
     public void SneakUpgrade()
     {
-        Debug.Log("Doged Upgrade");
+        upGrade[0].interactable = true;
+        upGrade[1].interactable = true;
+        upGrade[2].interactable = false;
+        upGrade[3].interactable = true;
+        info.text = "3";
+    }
+    public void UpGrade()
+    {
+        upGrade[0].interactable = true;
+        upGrade[1].interactable = true;
+        upGrade[2].interactable = true;
+        upGrade[3].interactable = false;
+        info.text = "";
     }
     public void CloseWindow()
     {
