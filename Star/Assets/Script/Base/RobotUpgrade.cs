2069fba09164819448c76765db40307b3ee8d687
jackson0731 <annf731@gmail.com>
2024-02-23 12:05:35 +0800

    機器人升級

--- Star/Assets/Script/Base/RobotUpgrade.cs
+++ Star/Assets/Script/Base/RobotUpgrade.cs
@@ -1,10 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RobotUpgrade : MonoBehaviour
 {
     public Canvas canvas;
+    public Button[] upGrade;
+    public Text info;
+    public void HpUpgrade()
+    {
+        upGrade[0].interactable = false;
+        upGrade[1].interactable = true;
+        upGrade[2].interactable = true;
+        upGrade[3].interactable = true;
+        info.text = "1";
+    }
+    public void CollectUpgrade()
+    {
+        upGrade[0].interactable = true;
+        upGrade[1].interactable = false;
+        upGrade[2].interactable = true;
+        upGrade[3].interactable = true;
+        info.text = "2";
+    }
+    public void SneakUpgrade()
+    {
+        upGrade[0].interactable = true;
+        upGrade[1].interactable = true;
+        upGrade[2].interactable = false;
+        upGrade[3].interactable = true;
+        info.text = "3";
+    }
+    public void UpGrade()
+    {
+        upGrade[0].interactable = true;
+        upGrade[1].interactable = true;
+        upGrade[2].interactable = true;
+        upGrade[3].interactable = false;
+        info.text = "";
+    }
     public void CloseWindow()
     {
         gameObject.SetActive(false);
